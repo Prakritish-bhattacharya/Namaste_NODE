22defe3e66d69605c0078a0c7d387cc32512880c
prakritish <bhattacharyaprakritish@gmail.com>
2026-01-12 21:19:09 +0530

    Initial commit - Namaste Node basics

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 require("./xyz.js");
+// const calculateSum = require("./sum.js");
+const obj = require("./sum.js")
 
+obj.Subtraction(30,50)
+obj.Sum(30,50)
 console.log(globalThis === global);
+// calculateSum(3,4)
---
+++ sum.js
@@ -0,0 +1,14 @@
+console.log("I am SUM.js page")
+
+function calculateSum(a,b){
+    const sum = a+b
+    console.log(sum)
+}
+function calculateSubtraction(a,b){
+    const subtraction = a-b
+    console.log(subtraction)
+}
+module.exports = {
+    Sum : calculateSum,
+    Subtraction : calculateSubtraction
+}
